cfec3bf8ad3c22e3965e71c975080d25df7a0855
macro cosm <macro cosm>
2018-01-04 19:55:27 +0300

    creat page login, about

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from './components/LoginPage';
+import AboutPage from './components/AboutPage';
+
+function App() {
+    return (
+        <LoginPage />
+    )
+}
+
+export default App
+
